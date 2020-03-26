8770dc2cbebcaad1fa1cadb285bbb94330c5e352
andre hogenkamp <tikaltikal@gmail.com>
2020-03-26 18:39:05 +0100

    Initial commit

---
+++ src/main/java/nl/tikal/svg/create/SvgCreator.java
@@ -0,0 +1,12 @@
+package nl.tikal.svg.create;
+
+/**
+ * Hello world!
+ *
+ */
+public class SvgCreator
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/nl/tikal/svg/create/SvgCreatorTest.java
@@ -0,0 +1,21 @@
+package nl.tikal.svg.create;
+
+import static org.junit.Assert.assertTrue;
+
+import nl.tikal.svg.create.SvgCreator;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SvgCreatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        SvgCreator.main(new String[]{"Hello world"});
+    }
+}
